4ebd54f4be6d26f023f8414275f5652a6500a98a
Liquelaliqour <lewismomanyi34@gmail.com>
2024-06-18 15:59:34 +0300

    database and registration form

--- afyaboraproject/stackusers/views.py
+++ afyaboraproject/stackusers/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import   UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegistrationForm
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account successfully created for {username}! Login Now')
-            return render(request, 'home.html')
+            return render(request, 'stackbase:home')
     else:
-        form = UserCreationForm()
+        form = UserRegistrationForm()
         
     return render(request, 'stackusers/register.html', {'form': form})

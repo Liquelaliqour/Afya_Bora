3ed95996900f75374ba0b41c15af1e5116dfe8db
Liquelaliqour <lewismomanyi34@gmail.com>
2024-06-18 11:46:45 +0300

    registeration file

--- afyaboraproject/stackusers/views.py
+++ afyaboraproject/stackusers/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import   UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account successfully created for {username}! Login Now')
+            return render(request, 'home.html')
+    else:
+        form = UserCreationForm()
+        
     return render(request, 'stackusers/register.html', {'form': form})

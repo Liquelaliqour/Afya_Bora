fc1204e57df75b7d7edb560e2f6d93a444126c80
Liquelaliqour <lewismomanyi34@gmail.com>
2024-06-19 10:15:44 +0300

    working on the login usesr experience by denying access to certain pages once a user has logged in.

--- afyaboraproject/stackusers/views.py
+++ afyaboraproject/stackusers/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required
 def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
